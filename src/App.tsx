3e89ddc5009eb602a458620256f9a0d2c8d3d646
Fernando Brino <fernandobrino11@gmail.com>
2022-10-20 21:54:09 -0300

    DefaultLayout and Router Created

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import {ThemeProvider} from 'styled-components'
+import { Router } from './Router'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export const App = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <Router />
       <GlobalStyle />
     </ThemeProvider>
   )
